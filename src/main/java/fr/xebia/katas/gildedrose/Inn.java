ed38d55208c590d70d8cc731b01dae55d33ac207
Ellene <ellene.dijoux@gmail.com>
2012-02-01 21:46:19 +0100

    Code Story : add conjured feature

--- src/main/java/fr/xebia/katas/gildedrose/Inn.java
+++ src/main/java/fr/xebia/katas/gildedrose/Inn.java
@@ -4,90 +4,114 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Inn {
-	private List<Item> items;
+	private List<AdvancedItem> items;
 
 	public Inn() {
-		items = new ArrayList<Item>();
-		items.add(new Item("+5 Dexterity Vest", 10, 20));
-		items.add(new Item("Aged Brie", 2, 0));
-		items.add(new Item("Elixir of the Mongoose", 5, 7));
-		items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+		items = new ArrayList<AdvancedItem>();
+		items.add(new AdvancedItem("+5 Dexterity Vest", 10, 20));
+		items.add(new AdvancedItem("Aged Brie", 2, 0));
+		items.add(new AdvancedItem("Elixir of the Mongoose", 5, 7));
+		items.add(new AdvancedItem("Sulfuras, Hand of Ragnaros", 0, 80));
 		items
-				.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15,
+				.add(new AdvancedItem("Backstage passes to a TAFKAL80ETC concert", 15,
 						20));
-		items.add(new Item("Conjured Mana Cake", 3, 6));
+		items.add(new AdvancedItem("Conjured Mana Cake", 3, 6));
 	}
 
 	public void updateQuality() {
 		for (int i = 0; i < items.size(); i++) {
-			if (!items.get(i).getName().equals("Aged Brie")
-					&& !items.get(i).getName().equals(
-							"Backstage passes to a TAFKAL80ETC concert")) {
-				if (items.get(i).getQuality() > 0) {
-					if (!items.get(i).getName().equals(
-							"Sulfuras, Hand of Ragnaros")) {
-						items.get(i).setQuality(items.get(i).getQuality() - 1);
-					}
-				}
-			} else {
-				if (items.get(i).getQuality() < 50) {
-					items.get(i).setQuality(items.get(i).getQuality() + 1);
-
-					if (items.get(i).getName().equals(
-							"Backstage passes to a TAFKAL80ETC concert")) {
-						if (items.get(i).getSellIn() < 11) {
-							if (items.get(i).getQuality() < 50) {
-								items.get(i).setQuality(
-										items.get(i).getQuality() + 1);
-							}
-						}
-
-						if (items.get(i).getSellIn() < 6) {
-							if (items.get(i).getQuality() < 50) {
-								items.get(i).setQuality(
-										items.get(i).getQuality() + 1);
-							}
-						}
-					}
-				}
-			}
-
-			if (!items.get(i).getName().equals("Sulfuras, Hand of Ragnaros")) {
-				items.get(i).setSellIn(items.get(i).getSellIn() - 1);
-			}
-
-			if (items.get(i).getSellIn() < 0) {
-				if (!items.get(i).getName().equals("Aged Brie")) {
-					if (!items.get(i).getName().equals(
-							"Backstage passes to a TAFKAL80ETC concert")) {
-						if (items.get(i).getQuality() > 0) {
-							if (!items.get(i).getName().equals(
-									"Sulfuras, Hand of Ragnaros")) {
-								items.get(i).setQuality(
-										items.get(i).getQuality() - 1);
-							}
-						}
-					} else {
-						items.get(i).setQuality(
-								items.get(i).getQuality()
-										- items.get(i).getQuality());
-					}
-				} else {
-					if (items.get(i).getQuality() < 50) {
-						items.get(i).setQuality(items.get(i).getQuality() + 1);
-					}
-				}
-			}
+			AdvancedItem item = items.get(i);
+			updateAdvancedItemQuality(item);
 		}
 
 	}
 
-	public List<Item> getItems() {
+	private void updateAdvancedItemQuality(AdvancedItem item) {
+		// quality decrease
+		if (!item.getName().equals("Aged Brie")
+				&& !item.getName().equals(
+						"Backstage passes to a TAFKAL80ETC concert")) {
+			if (item.getQuality() > 0) {
+				if (!item.getName().equals(
+						"Sulfuras, Hand of Ragnaros")) {
+					decreaseQuality(item);
+					if(item.getName().equals(
+						"Conjured")) {
+						decreaseQuality(item);
+					}
+				} 
+			}
+		} else {
+			//quality increase
+			if (item.getQuality() < 50) {
+				increaseQuality(item);
+
+				if (item.getName().equals(
+						"Backstage passes to a TAFKAL80ETC concert")) {
+					increaseBackstageItemQuality(item);
+				}
+			}
+		}
+
+		decreaseSellIn(item);
+
+		// sell date has passed
+		if (item.getSellIn() < 0) {
+			if (!item.getName().equals("Aged Brie")) {
+				if (!item.getName().equals(
+						"Backstage passes to a TAFKAL80ETC concert")) {
+					if (item.getQuality() > 0) {
+						if (!item.getName().equals(
+								"Sulfuras, Hand of Ragnaros")) {
+							decreaseQuality(item);
+						}
+					}
+				} else {
+					item.setQuality(0);
+				}
+			} else {
+				if (item.getQuality() < 50) {
+					increaseQuality(item);
+				}
+			}
+		}
+	}
+	
+
+	private void decreaseSellIn(AdvancedItem item) {
+		if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+			item.setSellIn(item.getSellIn() - 1);
+		}
+	}
+
+	private void increaseBackstageItemQuality(AdvancedItem item) {
+		if (item.getSellIn() < 11) {
+			if (item.getQuality() < 50) {
+				increaseQuality(item);
+			}
+		}
+
+		if (item.getSellIn() < 6) {
+			if (item.getQuality() < 50) {
+				increaseQuality(item);
+			}
+		}
+	}
+
+	private void increaseQuality(AdvancedItem item) {
+		item.setQuality(item.getQuality() + 1);
+	}
+
+	private void decreaseQuality(AdvancedItem item) {
+		item.setQuality(item.getQuality() - 1);
+	}
+
+	public List<AdvancedItem> getAdvancedItems() {
 		return items;
 	}
 	
 
-	public void setItems(List<Item> items) {
+	public void setAdvancedItems(List<AdvancedItem> items) {
 		this.items = items;
 	}
 
--- src/test/java/fr/xebia/katas/gildedrose/InnTest.java
+++ src/test/java/fr/xebia/katas/gildedrose/InnTest.java
@@ -3,6 +3,7 @@ package fr.xebia.katas.gildedrose;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.fest.assertions.Assertions.assertThat;
@@ -18,8 +19,8 @@ public class InnTest {
 
 	@Test
 	public void quality_should_degrade_twice_when_sell_date_passed() {
-		Item item = new Item("Elixir of the Mongoose", 0, 10);
-		setItem(item);
+		AdvancedItem item = new AdvancedItem("Elixir of the Mongoose", 0, 10);
+		setAdvancedItem(item);
 
 		inn.updateQuality();
 
@@ -28,8 +29,8 @@ public class InnTest {
 
 	@Test
 	public void quality_should_never_become_negative() {
-		Item item = new Item("Elixir of the Mongoose", 10, 0);
-		setItem(item);
+		AdvancedItem item = new AdvancedItem("Elixir of the Mongoose", 10, 0);
+		setAdvancedItem(item);
 
 		inn.updateQuality();
 
@@ -39,8 +40,8 @@ public class InnTest {
 
 	@Test
 	public void aged_brie_quality_should_increase() {
-		Item item = new Item("Aged Brie", 10, 20);
-		setItem(item);
+		AdvancedItem item = new AdvancedItem("Aged Brie", 10, 20);
+		setAdvancedItem(item);
 		
 		inn.updateQuality();
 		
@@ -49,8 +50,8 @@ public class InnTest {
 	
 	@Test
 	public void quality_should_never_exceed_50() {
-		Item item = new Item("Aged Brie", 10, 50);
-		setItem(item);
+		AdvancedItem item = new AdvancedItem("Aged Brie", 10, 50);
+		setAdvancedItem(item);
 		
 		inn.updateQuality();
 		
@@ -59,8 +60,8 @@ public class InnTest {
 	
 	@Test
 	public void sulfuras_quality_should_be_unchanged() {
-		Item item = new Item("Sulfuras, Hand of Ragnaros", 0, 25);
-		setItem(item);
+		AdvancedItem item = new AdvancedItem("Sulfuras, Hand of Ragnaros", 0, 25);
+		setAdvancedItem(item);
 		
 		inn.updateQuality();
 		
@@ -69,8 +70,8 @@ public class InnTest {
 	
 	@Test
 	public void sulfuras_sellIn_should_be_unchanged() {
-		Item item = new Item("Sulfuras, Hand of Ragnaros", 10, 25);
-		setItem(item);
+		AdvancedItem item = new AdvancedItem("Sulfuras, Hand of Ragnaros", 10, 25);
+		setAdvancedItem(item);
 		
 		inn.updateQuality();
 		
@@ -79,8 +80,8 @@ public class InnTest {
 	
 	@Test
 	public void backstage_quality_should_increase_when_sellIn_more_than_10() {
-		Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20);
-		setItem(item);
+		AdvancedItem item = new AdvancedItem("Backstage passes to a TAFKAL80ETC concert", 11, 20);
+		setAdvancedItem(item);
 		
 		inn.updateQuality();
 		
@@ -89,8 +90,8 @@ public class InnTest {
 	
 	@Test
 	public void backstage_quality_should_increase_by_2_when_sellIn_less_or_equal_to_10() {
-		Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20);
-		setItem(item);
+		AdvancedItem item = new AdvancedItem("Backstage passes to a TAFKAL80ETC concert", 10, 20);
+		setAdvancedItem(item);
 		
 		inn.updateQuality();
 		
@@ -99,8 +100,8 @@ public class InnTest {
 	
 	@Test
 	public void backstage_quality_should_increase_by_3_when_sellIn_less_or_equal_to_5() {
-		Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20);
-		setItem(item);
+		AdvancedItem item = new AdvancedItem("Backstage passes to a TAFKAL80ETC concert", 5, 20);
+		setAdvancedItem(item);
 		
 		inn.updateQuality();
 		
@@ -109,18 +110,29 @@ public class InnTest {
 	
 	@Test
 	public void backstage_quality_should_drop_to_0_when_sellIn_is_0() {
-		Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20);
-		setItem(item);
+		AdvancedItem item = new AdvancedItem("Backstage passes to a TAFKAL80ETC concert", 0, 20);
+		setAdvancedItem(item);
 		
 		inn.updateQuality();
 		
 		assertThat(item.getQuality()).isEqualTo(0);
 	}
 	
-	private void setItem(Item item) {
-		List<Item> items = new ArrayList<Item>();
+
+	@Test
+	public void conjured_quality_should_decrease_twice_than_others() {
+		AdvancedItem item = new AdvancedItem("Conjured", 10, 20);
+		setAdvancedItem(item);
+		
+		inn.updateQuality();
+		
+		assertThat(item.getQuality()).isEqualTo(18);
+	}
+	
+	private void setAdvancedItem(AdvancedItem item) {
+		List<AdvancedItem> items = new ArrayList<AdvancedItem>();
 		items.add(item);
-		inn.setItems(items);
+		inn.setAdvancedItems(items);
 	}
 
 
